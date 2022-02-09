40ff727617aa1fe15883a4507714f5841379ff15
Rimantas <belauzas@yahoo.com>
2022-02-09 13:12:11 +0200

    Initial commit

---
+++ js/pages/home.js
@@ -0,0 +1,39 @@
+// IMPORTS
+
+// CODE EXECUTION BELOW THIS COMMENT LINE
+
+/* header start */
+/* header end */
+
+/* hero start */
+/* hero end */
+
+/* top logos start */
+/* top logos end */
+
+/* about cards start */
+/* about cards end */
+
+/* about progress-bars start */
+/* about progress-bars end */
+
+/* services start */
+/* services end */
+
+/* why choose us start */
+/* why choose us end */
+
+/* contact start */
+/* contact end */
+
+/* blog start */
+/* blog end */
+
+/* testimonials start */
+/* testimonials end */
+
+/* bottom logos start */
+/* bottom logos end */
+
+/* footer start */
+/* footer end */
