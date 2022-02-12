3fdd7913d033d3b500acdce92620b959c8c1a24f
NoCastel <castelendy@gmail.com>
2022-02-13 00:04:38 +0200

    responsive menu size

--- js/pages/home.js
+++ js/pages/home.js
@@ -3,6 +3,16 @@
 // CODE EXECUTION BELOW THIS COMMENT LINE
 
 /* header start */
+/*~~~~~~~~~Responsive meniu 1000-1199px~~~~~~~~~*/
+const mediaQuery = window.matchMedia('(min-width: 1199px)');
+const resizeTopMenu = function () {
+    if (mediaQuery.matches) {
+        document.getElementsByTagName(`nav`)[0].className = `col-7 ml-3`;
+    } else {
+        document.getElementsByTagName(`nav`)[0].className = `col-8 ml-2`;
+    }
+};
+window.addEventListener('resize', resizeTopMenu);
 /* header end */
 
 /* hero start */
