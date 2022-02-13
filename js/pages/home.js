04c487f45645e82e6701ad0857b0e6bf43102852
NoCastel <castelendy@gmail.com>
2022-02-13 17:44:56 +0200

    responsive some updates

--- js/pages/home.js
+++ js/pages/home.js
@@ -4,15 +4,44 @@
 
 /* header start */
 /*~~~~~~~~~Responsive meniu 1000-1199px~~~~~~~~~*/
-const mediaQuery = window.matchMedia('(min-width: 1199px)');
-const resizeTopMenu = function () {
+const resizeTopMenu1199 = function () {
+    const mediaQuery = window.matchMedia('(min-width: 1199px)');
+    const e = document.getElementsByTagName(`nav`);
     if (mediaQuery.matches) {
-        document.getElementsByTagName(`nav`)[0].className = `col-7 ml-3`;
+        e[0].className = `col-7 ml-3`;
     } else {
-        document.getElementsByTagName(`nav`)[0].className = `col-8 ml-2`;
+        e[0].className = `col-8 ml-2`;
     }
 };
-window.addEventListener('resize', resizeTopMenu);
+/*~~~~~~~~~Hide menu~~~~~~~~~*/
+const hideNavigation = function () {
+    const maxWidth990 = window.matchMedia('(max-width: 990px)');
+    const e = document.getElementsByClassName(`navigation-bar`);
+    if (maxWidth990.matches|| window.innerWidth < 990) {
+        e[0].className = `hidden navigation-bar`;
+    } else {
+        e[0].className = `visible-flex navigation-bar`;
+    }
+};
+/*~~~~~~~~~Load functions after reload~~~~~~~~~*/
+window.onload = function () {
+    resizeTopMenu1199();
+    hideNavigation();
+};
+/*~~~~~~~~~Respond to resizing~~~~~~~~~*/
+window.addEventListener('resize', resizeTopMenu1199);
+window.addEventListener('resize', hideNavigation);
+
+// function openSideMenu() {
+//     const navTag = document.getElementsByTagName(`nav`);
+//     navTag[0].className = `col-5 menu-top-sidebar`;
+//     console.log("valio")
+//     // document.getElementsByTagName(`nav`).style.width = `250px`;
+// }
+// function closeSideMenu() {
+//     document.getElementsByTagName(`nav`).style.width = `0`;
+// }
+
 /* header end */
 
 /* hero start */
