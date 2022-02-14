09d8264a5ed31d08391ea270d700127b21e494c5
NoCastel <castelendy@gmail.com>
2022-02-15 00:51:07 +0200

    initial sidebar

--- js/pages/home.js
+++ js/pages/home.js
@@ -5,70 +5,76 @@
 /* header start */
 
 /*~~~~~~~~~Hide menu~~~~~~~~~*/
-// const hideNavigation = function () {
-//     const maxWidth990 = window.matchMedia('(max-width: 990px)');
-//     const navBar = document.getElementsByClassName(`navigation-bar`);
-//     if (maxWidth990.matches || window.innerWidth < 990) {
-//         navBar[0].className = `hidden navigation-bar`;
-//     } else {
-//         navBar[0].className = `visible-flex navigation-bar`;
-//     }
-// };
+const hideNavigation = function () {
+    const maxWidth990 = window.matchMedia('(max-width: 990px)');
+    const nav = document.getElementsByClassName(
+        `col-9 ml-1 col-lg-7 ml-lg-3 top-main-nav`
+    );
+    const navBar = document.getElementsByClassName(`navigation-bar`);
+    if (maxWidth990.matches || window.innerWidth < 990) {
+        navBar[0].className = `hidden navigation-bar`;
+        nav[0].className = `ml-10 top-main-nav`;
+    } else {
+        navBar[0].className = `visible-flex navigation-bar`;
+        nav[0].className = `col-9 ml-1 col-lg-7 ml-lg-3 top-main-nav`;
+    }
+    console.log(nav[0].classList);
+};
 /*~~~~~~~~~Load functions after reload~~~~~~~~~*/
-// window.onload = function () {
-//     resizeTopMenu1199();
-//     hideNavigation();
-// };
+window.onload = function () {
+    hideNavigation();
+};
+
+
 /*~~~~~~~~~Respond to resizing~~~~~~~~~*/
-// window.addEventListener('resize', resizeTopMenu1199);
-// window.addEventListener('resize', hideNavigation);
+window.addEventListener('resize', hideNavigation);
 /*~~~~~~~~~Sidebar Menu~~~~~~~~~*/
 
 // eslint-disable-next-line no-unused-vars
-// function openSideMenu() {
-//     // const nav = document.getElementsByTagName(`nav`);
-//     const nav = document.getElementsByClassName(`navigation-bar`);
-//     const navBar = document.getElementsByClassName(`navigation-bar`);
-//     const hid = document.getElementsByClassName(`top-menu-hidden`);
-//     const inner = document.getElementsByClassName(`top-menu-inner`);
-//     const menuOn = document.getElementsByClassName(`top-side-menu-on`);
-//     const menuOff = document.getElementsByClassName(`top-side-menu-off`);
-//     nav[0].className = `nav-side`;
-//     navBar[0].classList.add(`navigation-bar-side`);
-//     navBar[0].classList.remove(`hidden`);
-//     navBar[0].classList.remove(`navigation-bar`);
-//     for (let i = 0; i < 2; i++) {
-//         hid[0].classList.add(`top-menu-hidden-side`);
-//         hid[0].classList.remove(`top-menu-hidden`);
-//         inner[0].classList.add(`top-menu-inner-side`);
-//         inner[0].classList.remove(`top-menu-inner`);
-//     }
-//     menuOn[0].classList.add(`hidden`);
-//     menuOff[0].classList.remove(`hidden`);
-// }
+function openSideMenu() {
+    // const nav = document.getElementsByTagName(`nav`);
+    const nav = document.getElementsByClassName(`navigation-bar`);
+    const navBar = document.getElementsByClassName(`navigation-bar`);
+    const hid = document.getElementsByClassName(`top-menu-hidden`);
+    const inner = document.getElementsByClassName(`top-menu-inner`);
+    const menuOn = document.getElementsByClassName(`top-side-menu-on`);
+    const menuOff = document.getElementsByClassName(`top-side-menu-off`);
+    nav[0].className = `nav-side`;
+    navBar[0].classList.add(`navigation-bar-side`);
+    navBar[0].classList.remove(`hidden`);
+    navBar[0].classList.remove(`navigation-bar`);
+    for (let i = 0; i < 2; i++) {
+        hid[0].classList.add(`top-menu-hidden-side`);
+        hid[0].classList.remove(`top-menu-hidden`);
+        inner[0].classList.add(`top-menu-inner-side`);
+        inner[0].classList.remove(`top-menu-inner`);
+    }
+    menuOn[0].classList.add(`hidden`);
+    menuOff[0].classList.remove(`hidden`);
+}
 // eslint-disable-next-line no-unused-vars
-// function closeSideMenu() {
-//     // const nav = document.getElementsByTagName(`nav`);
-//     const navBar = document.getElementsByClassName(`navigation-bar`);
-//     const hid = document.getElementsByClassName(`top-menu-hidden-side`);
-//     const inner = document.getElementsByClassName(`top-menu-inner-side`);
-//     const menuOn = document.getElementsByClassName(`top-side-menu-on`);
-//     const menuOff = document.getElementsByClassName(`top-side-menu-off`);
+function closeSideMenu() {
+    // const nav = document.getElementsByTagName(`nav`);
+    const navBar = document.getElementsByClassName(`navigation-bar`);
+    const hid = document.getElementsByClassName(`top-menu-hidden-side`);
+    const inner = document.getElementsByClassName(`top-menu-inner-side`);
+    const menuOn = document.getElementsByClassName(`top-side-menu-on`);
+    const menuOff = document.getElementsByClassName(`top-side-menu-off`);
 
-//     // nav[0].className = `col-8 ml-2`;
-//     // nav[0].classList.remove(`nav-side`);
-//     navBar[0].classList.add(`navigation-bar`);
-//     navBar[0].classList.add(`hidden`);
-//     navBar[0].classList.remove(`navigation-bar-side`);
-//     for (let i = 0; i < hid[0].classList.length; i++) {
-//         hid[0].classList.add(`top-menu-hidden`);
-//         hid[0].classList.remove(`top-menu-hidden-side`);
-//         inner[0].classList.add(`top-menu-inner`);
-//         inner[0].classList.remove(`top-menu-inner-side`);
-//     }
-//     menuOff[0].classList.add(`hidden`);
-//     menuOn[0].classList.remove(`hidden`);
-// }
+    // nav[0].className = `col-8 ml-2`;
+    // nav[0].classList.remove(`nav-side`);
+    navBar[0].classList.add(`navigation-bar`);
+    navBar[0].classList.add(`hidden`);
+    navBar[0].classList.remove(`navigation-bar-side`);
+    for (let i = 0; i < hid[0].classList.length; i++) {
+        hid[0].classList.add(`top-menu-hidden`);
+        hid[0].classList.remove(`top-menu-hidden-side`);
+        inner[0].classList.add(`top-menu-inner`);
+        inner[0].classList.remove(`top-menu-inner-side`);
+    }
+    menuOff[0].classList.add(`hidden`);
+    menuOn[0].classList.remove(`hidden`);
+}
 // openSideMenu();
 /* header end */
 
